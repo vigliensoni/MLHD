38fd57f6fecb17a855d8e8aeae8e0aac8e546e7c
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2017-03-14 17:50:26 -0400

    New: script to go add creation date to CSV file with users’ metadata

---
+++ scripts/Python/1_add_creation_date_to_metadata/1_add_creation_date.py
@@ -0,0 +1,39 @@
+import os
+import gzip
+import GVM_classes
+import Features
+
+class MetadataFeatureExtractor:
+    """Extract metadata information from first line of files."""
+
+    def metadata(self, user_filepath):
+        """Constructor given a filepath of a GZIP TXT file"""
+        userdata = gzip.GzipFile(user_filepath).readline()
+        return userdata
+
+
+
+folderpath = "/Volumes/ListenBUP1/9_ALL_BUP"
+files = GVM_classes.folder_iterator(folderpath)
+
+
+outputfile = open('/Users/gabriel/Downloads/metadata_with_creation_date.tsv', 'ab')
+
+
+for file in files:
+
+    # extracting creation date
+    f = GVM_classes.FileMetadata()
+    creationdate = f.birthdate(file)
+
+    # extracting first line of metatada
+    u = MetadataFeatureExtractor()
+    metadata = u.metadata(file)
+    # removes newline at the end
+    metadata = metadata.strip()
+    # append file creation date to metadata
+    metadata = '\t'.join([metadata, str(creationdate)])
+    print metadata
+    outputfile.write(metadata)
+    outputfile.write('\n')
+
